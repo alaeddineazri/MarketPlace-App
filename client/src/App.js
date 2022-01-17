827be7d7fcf05735234d53db070ff0f2a9997fc8
alaeddine <alaeddinazri@gmail.com>
2022-01-17 19:10:16 +0100

    Toast notifications

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@ import Login from './auth/Login';
 import Register from './auth/Register';
 import NavbarMenu from './components/NavbarMenu';
 import Home from './pages/Home';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';    
 
 function App() {
   return (
   <BrowserRouter>
     <NavbarMenu />
+    <ToastContainer />
     <Routes >
       
       <Route path="/" element={<Home />} />
--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,30 +1,45 @@
 import React, { useState } from 'react'
 import RegisterForm from '../components/RegisterForm'
-import axios from 'axios'       
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom';
+
+
 
 
 const Register = () => {
+    let navigate = useNavigate();
+
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const handelSubmit = async (e) => {
-        e.preventDefault() 
+
+
+    async function handelSubmit(e) {
+        e.preventDefault()
         try {
-            const res = axios.post('http://localhost:5050/api/register',{
+            const res = await axios.post('http://localhost:5050/api/register', {
                 name,
                 email,
                 password
 
             })
-            console.table("res register",res)
+            console.log("resssssss", res)
+            toast.success("Register success")
+            navigate("/login")
+
         } catch (error) {
             console.log(error)
+            toast.error(error.response.data)
+
+
+
+
+
         }
-        
+
     }
-
-
 
 
 
@@ -34,16 +49,16 @@ const Register = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
-                    <RegisterForm 
-                        handelSubmit={handelSubmit} 
-                        name={name} 
-                        setName={setName} 
-                        email={email} 
-                        setEmail={setEmail} 
-                        password={password} 
-                        setPassword={setPassword} 
+                        <RegisterForm
+                            handelSubmit={handelSubmit}
+                            name={name}
+                            setName={setName}
+                            email={email}
+                            setEmail={setEmail}
+                            password={password}
+                            setPassword={setPassword}
 
-                    />
+                        />
                     </div>
 
                 </div>

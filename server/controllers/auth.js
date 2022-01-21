6c9d760725be5801197d0f2aa60bb53d73543ecd
alaeddine <alaeddinazri@gmail.com>
2022-01-21 21:57:55 +0100

    jwt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,40 +1,51 @@
-const userSchema = require('../models/user');
+const User = require('../models/user')
+var jwt = require('jsonwebtoken')
 
-const register = async (req,res)=>{
-    const {name , email , password} =req.body
+
+const register = async (req, res) => {
+    const { name, email, password } = req.body
     // validation 
-    if(!name) return res.status(400).send("Name is required")
-    if(!password || password.length <6) return res.status(400).send("Password is required and should be more than 6 car")
-    let emailExist = await userSchema.findOne({email}).exec()
+    if (!name) return res.status(400).send("Name is required")
+    if (!password || password.length < 6) return res.status(400).send("Password is required and should be more than 6 car")
+    let emailExist = await User.findOne({ email }).exec()
     if (emailExist) return res.status(400).send("Email is taken ")
 
     //register
-    const user =new userSchema(req.body)
+    const user = new User(req.body)
     try {
         await user.save()
-        console.log("user created ",user)
+        console.log("user created ", user)
         return res.send("created success")
     } catch (error) {
-        console.log("creat user error" ,error)
+        console.log("creat user error", error)
         res.status(400).send('Error')
-    }    
+    }
 }
 
 
-const login= async(req,res)=>{
-    const {email,password}=req.body
+const login = async (req, res) => {
+    const { email, password } = req.body
     try {
-        let user=await userSchema.findOne({email}).exec()
-        if(!user) res.status(400).send("user with that email not found ")
-        user.comparePassword(password,(error,mathc)=>{
-            if(!match||error) return res.status(400).send("wrong password ")
-            console.log("jwt plz")
+        let user = await User.findOne({ email }).exec()
+        if (!user) res.status(400).send("user with that email not found ")
+        user.comparePassword(password, (error, match) => {
+            if (!match || error) return res.status(400).send("wrong password ")
+            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1w' })
+            res.json({ token, user :{
+                _id: user._id,
+                name:user.name,
+                email:user.email,
+                createdAt:user.createdAt,
+                updatedAt:user.updatedAt
+
+            } })
+            console.log("token & user" ,token , user)
         })
     } catch (error) {
-        console.log("login error",error)
+        console.log("login error", error)
         res.status(400).send("login failed")
     }
 }
 
 
-module.exports = {register , login}
+module.exports = { register, login }
